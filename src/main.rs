7e8719abac3d3e74b5276daeff7c95a81e33ff99
hem <hemdrpun10@gmail.com>
2023-01-13 15:11:34 +0545

    Done with Arrays

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ use std::string;
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+// mod tuples;
+// mod array;
+// mod vectors;
+mod conditionals;
 
 fn main() {
     // print::run();
     // vars::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    // tuples::run();
+    // array::run();
+    // vectors::run();
+    conditionals::run();
 }
